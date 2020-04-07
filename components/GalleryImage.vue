7d5bd653e764e88f3ddb0c3732314fb29afc0b16
Matias <duarte.mati@gmail.com>
2020-04-06 21:48:45 -0300

    - Gallery layout fixed

--- components/GalleryImage.vue
+++ components/GalleryImage.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
+  <div style="margin: 10px">
     <div class="gallery-title">
       Gallery
     </div>
-    <ul>
-      <li v-for="image in images" :key="JSON.stringify(image)">
+    <div class="images-container">
+      <div v-for="image in images" :key="JSON.stringify(image)">
         <img v-lazy="image" :alt="image">
-      </li>
-    </ul>
+      </div>
+    </div>
     <div v-if="!images.length" style="text-align: center">
       No images were found
     </div>
@@ -24,58 +24,10 @@ export default {
 </script>
 
 <style scoped>
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  li {
-    padding: 3px;
-    height: 40vh;
-    flex-grow: 1;
-  }
-
-  li:last-child {
-    flex-grow: 10;
-  }
-
-  img {
-    max-height: 100%;
-    min-width: 100%;
-    object-fit: cover;
-    vertical-align: bottom;
-  }
-
-  @media (max-aspect-ratio: 1/1) {
-    li {
-      height: 30vh;
-    }
-  }
-  @media (max-height: 480px) {
-    li {
-      height: 80vh;
-    }
-  }
-
-
-  @media (max-aspect-ratio: 1/1) and (max-width: 480px) {
-    ul {
-      flex-direction: row;
-    }
-
-    li {
-      height: auto;
-      width: 100%;
-    }
-    img {
-      width: 100%;
-      max-height: 75vh;
-      min-width: 0;
-    }
-  }
   .images-container {
     display: grid;
-    gap: 15px;
+    gap: 20px;
+    padding: 20px;
     justify-items: center;
     justify-content: center;
     grid-template-columns: repeat(auto-fit, 300px);
